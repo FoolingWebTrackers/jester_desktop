a5388d8098eba66192a28f640809abd884d2d854
Mehmet Murat Budak <91738477+mbudak21@users.noreply.github.com>
2024-11-21 23:42:44 +0300

    Bundler is now packageing server binary

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,13 +1,26 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use tauri_plugin_shell::ShellExt;
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[tauri::command]
+async fn ping(app: tauri::AppHandle) {
+  app
+    .shell()
+    .sidecar("binaries/server")
+    .unwrap()
+    .spawn()
+    .unwrap();
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
+        .invoke_handler(tauri::generate_handler![ping])
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
